2ad36c80fcfa84758efbe99c8039e0e4b0115787
Dahale Nagnath <dahale.nagnath@magicsw.com>
2016-08-24 17:01:03 +0530

    Updated Code

---
+++ CASSANDRA/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CASSANDRA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.Values = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.Values.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCity);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtFName);
+            this.groupBox1.Location = new System.Drawing.Point(48, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 375);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insert Data";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(166, 33);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(219, 20);
+            this.txtLastName.TabIndex = 9;
+            this.txtLastName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(29, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Last name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(29, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Email";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(166, 94);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(219, 20);
+            this.txtCity.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(166, 156);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(219, 20);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(29, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(29, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "First name";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(166, 222);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(219, 20);
+            this.txtFName.TabIndex = 13;
+            // 
+            // Values
+            // 
+            this.Values.Controls.Add(this.button2);
+            this.Values.Controls.Add(this.dataGridView1);
+            this.Values.Controls.Add(this.label5);
+            this.Values.Location = new System.Drawing.Point(490, 58);
+            this.Values.Name = "Values";
+            this.Values.Size = new System.Drawing.Size(504, 383);
+            this.Values.TabIndex = 1;
+            this.Values.TabStop = false;
+            this.Values.Text = "Values";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(468, 228);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DB data";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 586);
+            this.Controls.Add(this.Values);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cassandra CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Values.ResumeLayout(false);
+            this.Values.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.GroupBox Values;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+
+
+    }
+}
+
---
+++ CASSANDRA/Form1.cs
@@ -0,0 +1,105 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CASSANDRA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Cluster _cluster;
+        ISession _session;
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+             _session = _cluster.Connect("hr");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _session.Execute("insert into users (lastname, city, email, firstname) values ( '" +txtLastName.Text + "' ,'" + txtCity.Text + "' ,'" + txtEmail.Text + "' ,'" + txtFName.Text +"' )" );
+            MessageBox.Show("Record Inserted");
+            
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string abh = txtLastName.Text + "' ,'" + txtCity.Text + "' ,'" + txtEmail.Text + "' ,'" + txtFName.Text;
+            
+        }
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = true;
+
+            var a = _session.Execute("select * from users");
+            var rows = a.GetRows().ToList();
+
+            DataTable table = new DataTable();
+            table.Columns.Add("LName", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+            table.Columns.Add("FName", typeof(string));
+
+            foreach (var row in rows)
+            {
+                object[] array = new object[row.Length];
+                for(int i=0;i<row.Length;i++)
+                {
+                    array[i] = row[i];
+
+                }
+                table.Rows.Add(array);
+
+            }
+
+            dataGridView1.DataSource = table;
+
+           //var b = _session.Execute("select city from users");
+           //var c = _session.Execute("select email from users");
+           //var d = _session.Execute("select firstname from users");
+            
+            
+            //dataGridView1.ColumnCount = 4;
+            //dataGridView1.Columns[0].Name = "Lastname";
+            //dataGridView1.Columns[1].Name = "City";
+            //dataGridView1.Columns[2].Name = "Email";
+            //dataGridView1.Columns[3].Name = "Firstname";
+
+            //string[] row = new string[] {a.ToString(),b.ToString(),c.ToString(),d.ToString()};
+            //dataGridView1.Rows.Add(row);
+
+            txtLastName.Text = "";
+            txtCity.Text = "";
+            txtEmail.Text = "";
+            txtFName.Text = "";
+           
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
